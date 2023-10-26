ac591c7bfe879980c2e7ef30ea5c1132930af4e3
Pingvu <nhlpool27@gmail.com>
2023-10-26 12:33:59 +0200

    Add project files.

---
+++ uppgift_3_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int maxHyraPerDag = 950;
+        int timpris = 80;
+
+        Console.Write("Hur många timmar vill du hyra bilen: ");
+        int antalTimmar = int.Parse(Console.ReadLine());
+
+        if (antalTimmar <= 0)
+        {
+            Console.WriteLine("Du måste hyra bilen i minst en timme.");
+        }
+        else if (antalTimmar * timpris <= maxHyraPerDag)
+        {
+            int kostnad = antalTimmar * timpris;
+            Console.WriteLine("Kostnaden för att hyra bilen i " + antalTimmar + " timmar är " + kostnad + " kr.");
+        }
+        else
+        {
+            Console.WriteLine("Du kan inte hyra bilen i så många timmar eftersom maxhyran per dag är 950 kr.");
+        }
+    }
+}
